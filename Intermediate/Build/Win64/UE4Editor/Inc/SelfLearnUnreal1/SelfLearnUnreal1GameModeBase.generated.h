26434247928b378b0adc6b9825fa300e837ce724
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-09-19 21:16:00 +1200

    firstCommit

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/SelfLearnUnreal1/SelfLearnUnreal1GameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SELFLEARNUNREAL1_SelfLearnUnreal1GameModeBase_generated_h
+#error "SelfLearnUnreal1GameModeBase.generated.h already included, missing '#pragma once' in SelfLearnUnreal1GameModeBase.h"
+#endif
+#define SELFLEARNUNREAL1_SelfLearnUnreal1GameModeBase_generated_h
+
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_RPC_WRAPPERS
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASelfLearnUnreal1GameModeBase(); \
+	friend SELFLEARNUNREAL1_API class UClass* Z_Construct_UClass_ASelfLearnUnreal1GameModeBase(); \
+public: \
+	DECLARE_CLASS(ASelfLearnUnreal1GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/SelfLearnUnreal1"), NO_API) \
+	DECLARE_SERIALIZER(ASelfLearnUnreal1GameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASelfLearnUnreal1GameModeBase(); \
+	friend SELFLEARNUNREAL1_API class UClass* Z_Construct_UClass_ASelfLearnUnreal1GameModeBase(); \
+public: \
+	DECLARE_CLASS(ASelfLearnUnreal1GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/SelfLearnUnreal1"), NO_API) \
+	DECLARE_SERIALIZER(ASelfLearnUnreal1GameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASelfLearnUnreal1GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASelfLearnUnreal1GameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASelfLearnUnreal1GameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASelfLearnUnreal1GameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASelfLearnUnreal1GameModeBase(ASelfLearnUnreal1GameModeBase&&); \
+	NO_API ASelfLearnUnreal1GameModeBase(const ASelfLearnUnreal1GameModeBase&); \
+public:
+
+
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASelfLearnUnreal1GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASelfLearnUnreal1GameModeBase(ASelfLearnUnreal1GameModeBase&&); \
+	NO_API ASelfLearnUnreal1GameModeBase(const ASelfLearnUnreal1GameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASelfLearnUnreal1GameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASelfLearnUnreal1GameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASelfLearnUnreal1GameModeBase)
+
+
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_12_PROLOG
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_RPC_WRAPPERS \
+	SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_INCLASS \
+	SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SelfLearnUnreal1_Source_SelfLearnUnreal1_SelfLearnUnreal1GameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
