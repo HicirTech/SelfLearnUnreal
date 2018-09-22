f861597eaf82e290aedd5cd1e202b0066002647b
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-09-22 22:55:07 +1200

    C++ basic finished

--- BasicC++/LearnC++Basic/LearnC++Basic/FGame.cpp
+++ BasicC++/LearnC++Basic/LearnC++Basic/FGame.cpp
@@ -1,26 +1,126 @@
 #include "stdafx.h"
+#include <iostream>
 #include "FGame.h"
+#include <random>;
+
+using FText = std::string;
+using int32 = int;
+FGame::FGame()
+{
+	this->answer = "answer";
+	this->MyMaxTry = 5;
+	this->isWin = false;
+	this->currenyTry = 1;
+}
+
+FGame::FGame(FText word,int32 maxTry)
+{
+	this->answer = word;
+	this->MyMaxTry = maxTry;
+	this->isWin = false;
+	this->currenyTry = 1;
+}
+
+void FGame::playGame()
+{
+	do
+	{
+		this->Game();
+		this->Reset();
+	} while (this->AskToPlayAgain());
+}
 
 void FGame::Reset()
 {
+	this->currenyTry = 1;
+	this->isWin = false;
+}
+void FGame::Game()
+{
+	printWelcome();
+	std::cout << "Max turn is " << this->GetMaxTry() <<
+		std::endl;
+
+	while (this->GetCurrentTry() <= this->GetMaxTry())
+	{
+		std::cout << "Your are currently at turn "
+			<< this->GetCurrentTry() << std::endl;
+
+		FText userInput;
+		userInput = getGuest();
+		//this->Chcker(userInput);
+		std::cout << "You are ";
+		if (checkIsMatch(userInput))
+		{
+			std::cout << "right " << std::endl
+				<< "you win the game" << std::endl;
+			this->isWin = true;
+			break;
+		}
+		else
+		{
+			std::cout << "wrong " << std::endl;
+		}
+		this->currenyTry++;
+	}
 }
 
-int FGame::GetMaxTry()
+bcCount FGame::Chcker(FText text)
 {
-	return MyMaxTry;
+	bcCount counter;
+	for (int32 i = 0; i != text.length()&&i!=this->answer.length(); i++)
+	{
+		if (text.at(i) == this->answer.at(i))
+		{
+			counter.bulls++;
+			counter.cows++;
+		}
+	}
+	return counter;
 }
 
-int FGame::GetCurrentTry()
+FText FGame::getGuest() 
 {
-	return MyCurrenyTry;
+	FText userInput;
+	std::cout << "your input is : ";
+	std::getline(std::cin, userInput);
+	return userInput;
+}
+bool FGame::checkIsMatch(FText input)
+{
+	return this->answer.compare(input)==0;
+}
+bool FGame::AskToPlayAgain() const
+{
+	std::cout << "Play again? [y/n]";
+	FText again;
+	std::getline(std::cin, again);
+	return again.compare("y")==0;
 }
 
-bool FGame::CheckGuest(std::string input)
+void FGame::printWelcome() 
 {
-	return false;
+	std::cout << "Welcome tp guest word " << std::endl;
+	std::cout << "Word length is " << this->answer.length() <<
+		" hint : ";
+	FText copyOfCode = this->answer;
+	std::random_shuffle(copyOfCode.begin(), copyOfCode.end());
+	std::cout << copyOfCode << std::endl;
 }
 
-bool FGame::IsWon()
+
+bool FGame::IsWon() const
 {
-	return false;
+	return this->isWin;
+}
+
+
+int32 FGame::GetMaxTry() const
+{
+	return this->MyMaxTry;
+}
+
+int32 FGame::GetCurrentTry()const
+{
+	return this->currenyTry;
 }
--- BasicC++/LearnC++Basic/LearnC++Basic/FGame.h
+++ BasicC++/LearnC++Basic/LearnC++Basic/FGame.h
@@ -1,22 +1,38 @@
 #pragma once
 #include <string>
+using FText = std::string;
+using int32 = int;
+struct bcCount
+{
+	int32 bulls = 0;
+	int32 cows = 0;
+};
 class FGame
 {
-public :
-	void Reset();
-	int GetMaxTry();
-	int GetCurrentTry();
-	bool CheckGuest(std::string input);
-	bool IsWon();
 	
+	public :
+	
+		FGame();
+		FGame(FText word, int32 maxTry);
+		void playGame();
 
+	private: 
 
+		FText answer;
+		int32 currenyTry;
+		int32 MyMaxTry;
+		bool isWin;
 
-private: 
-	int MyCurrenyTry;
-	int MyMaxTry;
-	bool IsIsogram;
-
-
+		int GetCurrentTry() const;
+		int GetMaxTry() const;
+	
+		bool IsWon() const;
+		void Reset();
+		bool AskToPlayAgain() const ;
+		void printWelcome();
+		bool checkIsMatch(FText input);
+		FText getGuest();
+		void Game();
+		bcCount Chcker(FText);
 };
 
