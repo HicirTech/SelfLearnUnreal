ac6dbaf278dcdbefa7a24ed2fca08f1e9c947409
电子腋毛程序/Electronic hirci program <30775858+HicirTech@users.noreply.github.com>
2018-09-25 20:09:10 +1200

    fix sln

--- S1/Source/S1/PositionReport.cpp
+++ S1/Source/S1/PositionReport.cpp
@@ -1,15 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	
 	PrimaryComponentTick.bCanEverTick = true;
-
+	
 	// ...
 }
 
@@ -19,8 +20,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	GetOwner();
-	UE_LOG(LogTemp, Warning, TEXT("Posting report on chair"));
+	FString objectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Posting report on chair : %s"),*objectName);
 	// ...
 	
 }
